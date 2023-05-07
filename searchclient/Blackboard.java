3f84f7d3d3c120a3e4fbbdd5ae82e138bdb2b59e
PossibleNPC <28771786+PossibleNPC@users.noreply.github.com>
2023-05-07 12:50:02 +0200

    partial migration
    integration is next

---
+++ searchclient/Blackboard.java
@@ -0,0 +1,130 @@
+package searchclient;
+
+import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/*
+    This class is used to store all of the information that the agents and boxes have access to.
+    This includes the state of the world, the goals, and the agents and boxes. Somehow, it needs
+    to include a temporal aspect so conflicts can be detected and resolved.
+ */
+public class Blackboard {
+    // Because this is a WIP, I am going to use the domain model concepts to test how it would work. I think it should
+    // be okay from a memory perspective, but testing will show.
+    private static volatile Blackboard instance;
+    private static boolean isInitialized = false;
+    List<Agent> agents;
+    List<Box> boxes;
+    List<Goal> goals;
+    int width;
+    int height;
+    int[][] intMap;
+    double[][] dist;
+    // TODO: Use the new map representation with adjacent vertices
+    List<List<Vertex>> mapRepresentation;
+
+    // TODO: Investigate how we want to handle vertices to move boxes/agents blocking others
+    SortedSet<Vertex> reservedVertices = null;
+    SortedSet<Vertex> unreservedVertices = new TreeSet<>();
+
+    private Blackboard(List<Agent> agents, List<Box> boxes, List<Goal> goals,
+                       int width, int height, int[][] intMap, double[][] dist, List<List<Vertex>> mapRepresentation) {
+        this.agents = agents;
+        this.boxes = boxes;
+        this.goals = goals;
+        this.width = width;
+        this.height = height;
+        this.intMap = intMap;
+        this.dist = dist;
+        this.mapRepresentation = mapRepresentation;
+    }
+
+    public synchronized double getDistance( int startX, int startY, int endX, int endY) {
+        int startVertex = intMap[startX][startY];
+        int endVertex = intMap[endX][endY];
+
+        // TODO:
+        // This should be solved with using the adjacent vertices because we no longer put walls as -1; confirm that
+        // intMap won't give us this just be sure. Otherwise, we need a check for -1 here.
+
+//        if (startVertex == -1 || endVertex == -1) {
+//            // This a hack because of the less than logic it feeds
+//            return Integer.MAX_VALUE;
+//        }
+        return dist[startVertex][endVertex];
+    }
+
+    public static void initialize(List<Agent> agents, List<Box> boxes, List<Goal> goals, int width, int height
+    ,int[][] intMap, double[][] dist, List<List<Vertex>> mapRepresentation){
+        if(!isInitialized){
+            synchronized (Blackboard.class){
+                if (!isInitialized){
+                    instance = new Blackboard(agents, boxes,goals,width,height, intMap,dist, mapRepresentation);
+                    isInitialized = true;
+                }
+            }
+        }
+    }
+
+    // TODO: make this dynamic as tasks are completed and newly assigned
+    public synchronized void reserveVertices(List<Vertex> verticesToBeReserved){
+        if (reservedVertices == null){
+            reservedVertices = new TreeSet<>();
+        }
+        for (var vertex : verticesToBeReserved) {
+            reservedVertices.add(vertex);
+        }
+    }
+
+    // TODO: make this dynamic as tasks are completed and newly assigned
+    public synchronized void verticesNotReserved(){
+        for (var vertices : this.mapRepresentation) {
+            for (var vertex : vertices) {
+                if (!this.reservedVertices.contains(vertex)) {
+                    this.unreservedVertices.add(vertex);
+                }
+            }
+        }
+//        if (unreservedVertices == null){
+//            unreservedVertices = new TreeSet<>();
+//        }
+//        for (var vertex : verticesToBeUnreserved) {
+//            unreservedVertices.add(vertex);
+//        }
+    }
+
+    public synchronized Vertex getVertex(int x, int y){
+        // TODO : maybe it would be possible to implement a pointer reference system so that
+        //  any changes made by an agent or the centralized planner are reflected to any Vertex used in the solution.
+        return mapRepresentation.get(x).get(y);
+    }
+
+    public static Blackboard getInstance() {
+        if (!isInitialized) {
+            throw new IllegalStateException("Blackboard not initialized");
+        }
+        return instance;
+    }
+    public synchronized String toString() {
+        StringBuilder result = new StringBuilder("Agents: ");
+
+        for (Agent agent : agents) {
+            result.append(agent.toString()).append(", ");
+        }
+
+        result.append("Boxes: ");
+
+        for (Box box : boxes) {
+            result.append(box.toString()).append(", ");
+        }
+
+        result.append("Goals: ");
+
+        for (Goal goal : goals) {
+            result.append(goal.toString()).append(", ");
+        }
+
+        return result.toString();
+    }
+}
---
+++ searchclient/Preprocessing.java
@@ -0,0 +1,71 @@
+package searchclient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Preprocessing {
+    // This class is meant to be used to preprocess the map according to the domain model
+    // It should not be copied nor passed into any expanded nodes
+
+    // We are also going to have Preprocessing create the blackboard
+    public State initialState;
+
+
+    public Preprocessing(State initialState) {
+        this.initialState = initialState;
+    }
+
+    public List<Agent> findAgents() {
+        List<Agent> agents = new ArrayList<>();
+
+        for (int i = 0; i < this.initialState.agentRows.length; i++) {
+            agents.add(new Agent(Integer.toString(i), State.agentColors[i], initialState.agentRows[i], initialState.agentCols[i]));
+        }
+        return agents;
+    }
+
+    public List<Box> findBoxes() {
+        List<Box> boxes = new ArrayList<>();
+
+        for (int i = 0; i < initialState.boxes.length; i++) {
+            for (int j = 0; j < initialState.boxes[0].length; j++) {
+                if (Character.isLetter(initialState.boxes[i][j])) {
+                    boxes.add(new Box(i, j, String.valueOf(initialState.boxes[i][j]), State.boxColors[Box.toNumeric(String.valueOf(initialState.boxes[i][j]))]));
+                }
+            }
+        }
+
+        return boxes;
+    }
+
+    // TODO: Make these goals ordered
+    public List<Goal> findGoals() {
+        List<Goal> goals = new ArrayList<>();
+
+        // TODO: This marks goals ONLY as boxes, but not when they are agents moving to a location
+        for (int i = 0; i < State.goals.length; i++) {
+            for (int j = 0; j < State.goals[0].length; j++) {
+                if (Character.isLetter(State.goals[i][j])) {
+                    goals.add(new Goal(i, j, String.valueOf(State.goals[i][j]), State.boxColors[Box.toNumeric(String.valueOf(State.goals[i][j]))]));
+                }
+                if (Character.isDigit(State.goals[i][j])) {
+                    goals.add(new Goal(i, j, String.valueOf(State.goals[i][j]), null));
+                }
+            }
+        }
+        return goals;
+    }
+
+    public int findWidth() {
+        return State.walls[0].length;
+    }
+
+    public int findHeight() {
+        return State.walls.length;
+    }
+
+    public Blackboard initializeBlackboard(int[][] intMap, double[][] dist, List<List<Vertex>> mapRepresentation) {
+        Blackboard.initialize(findAgents(), findBoxes(), findGoals(), findWidth(), findHeight(), intMap, dist, mapRepresentation);
+        return Blackboard.getInstance();
+    }
+}
